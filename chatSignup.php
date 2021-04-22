61437a51a97a7b078aaf8b4bb4726cdc76f0954f
Oren Avni <orenavnimobile@gmail.com>
2021-04-22 10:33:04 +0300

    test rm images folder

--- chatSignup.php
+++ chatSignup.php
@@ -3,7 +3,7 @@
     
     header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
     header('Access-Control-Allow-Credentials: true');
-
+    /** @var $conn db_connection */
     $conn = OpenCon();
 
     $fname = $_POST["fname"];
